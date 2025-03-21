ac3d912905a498fbb276911e46b57a6c37dd907c
kyawMHtet <kyawmhtet23@gmail.com>
2025-03-21 14:44:36 +0700

    nav

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 
-  function myFunction() {
+function myFunction() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+    x.classList.toggle("responsive");
   }
+  
 
 window.addEventListener("scroll", function () {
     var navbar = document.getElementById("myTopnav");
